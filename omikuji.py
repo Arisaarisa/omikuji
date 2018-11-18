625e98491451a94d89b2edb144f3a3e0a0a4044a
arisa <arisankochira10@gmail.com>
2018-11-18 21:03:07 +0900

    名前を入力できるようにした。

--- omikuji.py
+++ omikuji.py
@@ -3,7 +3,10 @@ import random
 
 def print_omikuji():
     items = ['大吉', '吉', '凶']
-    print("今日の運勢は" + random.choice(items) + 'です。')
+    print(input_name + "の今日の運勢は" + random.choice( items) + 'です。')
+
+input_name =input("名前をおしえてください:")
+
 
 
 print_omikuji()
